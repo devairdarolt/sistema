4accb4b61cb2e9c96031cc5ee9516d9857d5b5e8
devairdarolt <devairdarolt@gmail.com>
2021-08-28 12:16:22 -0300

    Consulta de categorias (#1, #2, #4)
    Adicionado consulta de Categoria e Produtos relacionados
    Adicionado tratamento de erro para Serviço de busca por Categoria
    Adicionar erro padrão em Resource, busca por categoria

--- src/main/java/com/ecomerce/sistema/domain/Produto.java
+++ src/main/java/com/ecomerce/sistema/domain/Produto.java
@@ -12,19 +12,25 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+
 @Entity
 public class Produto implements Serializable {
-	
+
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	private Double preco;
 
+	// Para recursividade que chegou até esta classe. Ex.:
+	// Categoria{@JsonManagedReference} ----> Produto{@JsonBackReference} (stop)
+	@JsonBackReference
 	@ManyToMany
-	@JoinTable(name="PRODUTO_CATEGORIA", joinColumns = @JoinColumn(name="produto_id"), inverseJoinColumns = @JoinColumn(name="categoria_id"))
+	@JoinTable(name = "PRODUTO_CATEGORIA", joinColumns = @JoinColumn(name = "produto_id"), inverseJoinColumns = @JoinColumn(name = "categoria_id"))
 	private List<Categoria> categorias = new ArrayList<>();
 
 	public Produto() {
