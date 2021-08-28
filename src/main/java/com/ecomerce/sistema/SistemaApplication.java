148f4b73affc6932f7f1de71404092a672c5bea5
devairdarolt <devairdarolt@gmail.com>
2021-08-28 10:33:35 -0300

    Intancia do modelo conceitual issue:#4
    Adicionado instancias de valores para testes do modelo conceitual

--- src/main/java/com/ecomerce/sistema/SistemaApplication.java
+++ src/main/java/com/ecomerce/sistema/SistemaApplication.java
@@ -1,13 +1,36 @@
 package com.ecomerce.sistema;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SistemaApplication {
+import com.ecomerce.sistema.domain.Categoria;
+import com.ecomerce.sistema.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class SistemaApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SistemaApplication.class, args);
+	}
+
+	/**
+	 * Instancias do modelo conceitual {@link #2}
+	 */
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		
 	}
 
 }
