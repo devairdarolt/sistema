c0031d615ae073e6b53fc12973fd7744f356c0a5
devairdarolt <devairdarolt@gmail.com>
2021-08-31 17:22:32 -0300

    Pages Categoria  (#4)
    + Listando CategoriaDTO por pages
    + Adicionado mais instâncias do modelo conceitual (#2)

--- src/main/java/com/ecomerce/sistema/SistemaApplication.java
+++ src/main/java/com/ecomerce/sistema/SistemaApplication.java
@@ -45,13 +45,14 @@ public class SistemaApplication implements CommandLineRunner {
 	@Autowired
 	private ClienteRepository clienteRepository;
 	@Autowired
-	private EnderecoRepository enderecoRepository;	
+	private EnderecoRepository enderecoRepository;
 	@Autowired
-	private PedidoRepository pedidoRepository;	
+	private PedidoRepository pedidoRepository;
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
-	@Autowired ItemPedidoRepository itemPedidoRepository;
-	
+	@Autowired
+	ItemPedidoRepository itemPedidoRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(SistemaApplication.class, args);
 	}
@@ -67,6 +68,11 @@ public class SistemaApplication implements CommandLineRunner {
 		 */
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
+		Categoria cat3 = new Categoria(null, "Games");
+		Categoria cat4 = new Categoria(null, "Festas");
+		Categoria cat5 = new Categoria(null, "Eletrônicos");
+		Categoria cat6 = new Categoria(null, "Softwares");
+		Categoria cat7 = new Categoria(null, "Veículos");
 
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
@@ -79,7 +85,7 @@ public class SistemaApplication implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().add(cat1);
 
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 
 		/**
@@ -125,29 +131,30 @@ public class SistemaApplication implements CommandLineRunner {
 		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
 		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
 		ped1.setPagamento(pagto1);
-		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
-		ped2.setPagamento(pagto2);		
-		cli1.getPedidos().addAll(Arrays.asList(ped1,ped2));
-		
-		pedidoRepository.saveAll(Arrays.asList(ped1,ped2));
-		pagamentoRepository.saveAll(Arrays.asList(pagto1,pagto2));
-		clienteRepository.save(cli1); //NÃO É NECESSÁRIO
-		
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"),
+				null);
+		ped2.setPagamento(pagto2);
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
+		clienteRepository.save(cli1); // NÃO É NECESSÁRIO
+
 		/**
 		 * ITEM_PEDIDO
 		 */
 		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, p1.getPreco());
 		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, p3.getPreco());
 		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, p2.getPreco());
-		
-		ped1.getItens().addAll(Arrays.asList(ip1,ip2));
+
+		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
 		ped2.getItens().add(ip3);
-		
+
 		p1.getItens().add(ip1);
 		p2.getItens().addAll(Arrays.asList(ip3));
 		p3.getItens().addAll(Arrays.asList(ip2));
-		itemPedidoRepository.saveAll(Arrays.asList(ip1,ip2,ip3));
-		
+		itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
+
 	}
 
 }
