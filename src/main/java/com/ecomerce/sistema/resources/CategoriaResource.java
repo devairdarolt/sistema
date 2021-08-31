2a0c9572adf463de75a069fefa512aedcff68298
devairdarolt <devairdarolt@gmail.com>
2021-08-31 14:46:50 -0300

    Insert Categoria (#4)
    + Adicionado método para inserir nova Categoria
    + Adicionado Resource REST para adição de Categoria

--- src/main/java/com/ecomerce/sistema/resources/CategoriaResource.java
+++ src/main/java/com/ecomerce/sistema/resources/CategoriaResource.java
@@ -1,14 +1,17 @@
 package com.ecomerce.sistema.resources;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.ecomerce.sistema.domain.Categoria;
 import com.ecomerce.sistema.services.CategoriaService;
@@ -16,14 +19,26 @@ import com.ecomerce.sistema.services.CategoriaService;
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
-	
+
 	@Autowired
 	CategoriaService service;
-	
+
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public ResponseEntity<?> find(@PathVariable Integer id){
+	public ResponseEntity<?> find(@PathVariable Integer id) {
 		Categoria obj = service.buscar(id);
 		return ResponseEntity.ok().body(obj);
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
+		obj = service.insert(obj);
+		// Para retornar como resposta HTTP 201 a url com o Id ==
+		// http://localhost:8080/categorias/{id}
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+
+		// Cria o código HTTP 201 para informar que foi criado uma nova Categoria
+		return ResponseEntity.created(uri).build();
+
 	}
 
 }
