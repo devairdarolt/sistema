0daa86ce126e0a4ad67b7769e43a988fc022d3f4
devairdarolt <devairdarolt@gmail.com>
2021-08-31 15:38:02 -0300

    DELETE Categoria
    + Adicionado Resource REST para DELETE de Categoria
    + Adicionado tratamento de erro BAD_REQUEST 400 para Categorias que possuem Produtos e não podem ser apagadas

--- src/main/java/com/ecomerce/sistema/services/CategoriaService.java
+++ src/main/java/com/ecomerce/sistema/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.ecomerce.sistema.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.ecomerce.sistema.domain.Categoria;
 import com.ecomerce.sistema.repositories.CategoriaRepository;
+import com.ecomerce.sistema.services.exceptions.DataIntegrityException;
 import com.ecomerce.sistema.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -29,5 +31,15 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+		repo.deleteById(id);
+		}catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma Categoria que possui produtos");
+		}
+		
 	}
 }
