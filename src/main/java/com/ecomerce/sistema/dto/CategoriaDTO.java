720761dd8ff129507966f09bec3baefe4350171f
devairdarolt <devairdarolt@gmail.com>
2021-09-01 10:57:21 -0300

    CRUD Categoria (#4)
    + Finalizado GET, POST, PUT, DELETE para o objeto Categoria

--- src/main/java/com/ecomerce/sistema/dto/CategoriaDTO.java
+++ src/main/java/com/ecomerce/sistema/dto/CategoriaDTO.java
@@ -2,6 +2,8 @@ package com.ecomerce.sistema.dto;
 
 import java.io.Serializable;
 
+//import javax.validation.constraints.NotEmpty; Esta forma não funcionou o @NotEmpty
+
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotEmpty;
 
@@ -11,15 +13,15 @@ public class CategoriaDTO implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	private Integer id;
-	
+
 	@NotEmpty(message = "Preenchimento obrigatório")
 	@Length(min = 5, max = 50, message = "O tamanho deve estar entre 5 e 50 caracteres")
 	private String nome;
-	
+
 	public CategoriaDTO() {
-		
+
 	}
-	
+
 	public CategoriaDTO(Categoria obj) {
 		this.id = obj.getId();
 		this.nome = obj.getNome();
@@ -40,6 +42,5 @@ public class CategoriaDTO implements Serializable {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	
-	
+
 }
