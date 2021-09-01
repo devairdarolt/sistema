199fc60a426d58db2ebbb20a2dfe8e4c8f666fa2
devairdarolt <devairdarolt@gmail.com>
2021-09-01 13:02:10 -0300

    DELETE Cliente (#4)
    + Delete cascade endereços, apaga clientes que possuem endereços mas não possuem pedidos

--- src/main/java/com/ecomerce/sistema/domain/Cliente.java
+++ src/main/java/com/ecomerce/sistema/domain/Cliente.java
@@ -6,6 +6,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.CollectionTable;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
@@ -28,8 +29,10 @@ public class Cliente implements Serializable {
 	private String email;
 	private String cpfOuCnpj;
 	private Integer tipo;
-
-	@OneToMany(mappedBy = "cliente") // nome do objeto java
+	/**
+	 * cascade.all = Quando apagar cliente apagar os endereços relacionados
+	 */
+	@OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL) 
 	private List<Endereco> enderecos = new ArrayList<>();
 
 	// Entity <<weak>>
--- src/main/java/com/ecomerce/sistema/services/ClienteService.java
+++ src/main/java/com/ecomerce/sistema/services/ClienteService.java
@@ -61,7 +61,7 @@ public class ClienteService {
 		try {
 			repo.deleteById(id);
 		} catch (DataIntegrityViolationException e) {
-			throw new DataIntegrityException("Não é possível excluir um Cliente que possui entidades relacionadas");
+			throw new DataIntegrityException("Não é possível excluir um Cliente que possui pedodos relacionados");
 		}
 
 	}
