35ad8d2096968017a68b151e6c6b8fac2d289ade
devairdarolt <devairdarolt@gmail.com>
2021-09-01 13:56:22 -0300

    Validação ClienteInsert (#4)
    + Adicionado validação com verificação de E-mail ja existente no banco de dados

--- src/main/java/com/ecomerce/sistema/repositories/ClienteRepository.java
+++ src/main/java/com/ecomerce/sistema/repositories/ClienteRepository.java
@@ -2,10 +2,13 @@ package com.ecomerce.sistema.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.ecomerce.sistema.domain.Cliente;
 
 @Repository
-public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
 
+	@Transactional(readOnly = true) // Para fazer leituras rápidas do banco de dados sem Transaction Lock
+	Cliente findByEmail(String email);
 }
