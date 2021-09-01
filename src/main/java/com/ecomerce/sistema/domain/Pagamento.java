7022ec3b969ad4b5d6b886252fa35f499186e665
devairdarolt <devairdarolt@gmail.com>
2021-09-01 11:05:52 -0300

    Bug
    + Alterado instância de EstadoPagamento para evitar erro de conversão de DTO para Pagamento

--- src/main/java/com/ecomerce/sistema/domain/Pagamento.java
+++ src/main/java/com/ecomerce/sistema/domain/Pagamento.java
@@ -16,29 +16,28 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
-public abstract class Pagamento implements Serializable{
+public abstract class Pagamento implements Serializable {
 
-	
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	private Integer id;
 	private Integer estado;
-	
+
 	@JsonIgnore
 	@OneToOne
 	@JoinColumn(name = "pedido_id")
 	@MapsId
 	private Pedido pedido;
-	
-	public Pagamento(){
-		
+
+	public Pagamento() {
+
 	}
 
 	public Pagamento(Integer id, EstadoPagamento estado, Pedido pedido) {
 		super();
 		this.id = id;
-		this.estado = estado.getCod();
+		this.estado = (estado == null) ? null : estado.getCod();
 		this.pedido = pedido;
 	}
 
@@ -90,5 +89,5 @@ public abstract class Pagamento implements Serializable{
 			return false;
 		return true;
 	}
-	
+
 }
