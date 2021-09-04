9ae6fe0575876a7eeb7bb9a1bba0b1c16ec7611e
devairdarolt <devairdarolt@gmail.com>
2021-09-04 18:10:13 -0300

    Criado profile de teste

---
+++ src/main/java/com/ecommerce/sistema/config/TestConfig.java
@@ -0,0 +1,27 @@
+package com.ecommerce.sistema.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.ecommerce.sistema.services.DBService;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+	@Autowired
+	private DBService dBService;
+	
+	
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		
+		dBService.instantiateTestDatabase();		
+		return true;
+		
+	}
+
+}
