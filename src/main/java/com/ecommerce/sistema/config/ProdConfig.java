3b5764cabc650c1007cd18292acfb1719f792fa1
devairdarolt <devairdarolt@gmail.com>
2021-09-05 19:17:24 -0300

    Alterações de configurações

---
+++ src/main/java/com/ecommerce/sistema/config/ProdConfig.java
@@ -0,0 +1,33 @@
+package com.ecommerce.sistema.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.ecommerce.sistema.services.DBService;
+
+@Configuration
+@Profile("prod")
+public class DevConfig {
+	@Autowired
+	private DBService dBService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		
+		if("create".equals(strategy)) {
+			//dBService.instantiateTestDatabase();
+			return true;
+		}
+		return false;
+		
+	}
+
+}
